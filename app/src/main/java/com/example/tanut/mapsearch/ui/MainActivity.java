ab634198d2ab461375231070ef90966e795dd8a2
LAPTOP-EPUP2MKU\tanut <tanmaythakar4@gmail.com>
2017-10-19 20:56:45 -0400

    some change

--- app/src/main/java/com/example/tanut/mapsearch/ui/MainActivity.java
+++ app/src/main/java/com/example/tanut/mapsearch/ui/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Load MapFragment
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, MapFragment.newInstance(),MapFragment.TAG).commit();
